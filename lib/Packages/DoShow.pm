b688b16487ef2ca8ad7861d7c20da16a9f3f4448
Frank Lichtenheld <Frank Lichtenheld frank@lichtenheld.de>
2007-06-02 23:30:14 +0200

    templates/html/show.tmpl: Denote unofficial ports
    Get this information by 1) exposing the archive in the
    %downloads hash 2) using config/mirrors.tmpl to determine
    whether this archive is an unofficial port

--- lib/Packages/DoShow.pm
+++ lib/Packages/DoShow.pm
@@ -201,6 +201,7 @@ sub do_show {
 				      instsize => $sizes_inst->{$a}, );
 
 			    $d{version} = $versions->{$a} if $multiple_versions;
+			    $d{archive} = $archives->{$a};
 			    if ( ($suite ne "experimental")
 				 && ($subsection ne 'debian-installer')) {
 				$d{contents_avail} = 1;
